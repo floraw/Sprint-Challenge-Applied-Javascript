8c188f57e7f9ffe9005a01d400eded85ec8025f9
Flora Winters <flora.e.winters@gmail.com>
2019-12-15 18:10:28 -0800

    Carousel creator function, outline of functionality

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -17,3 +17,43 @@
     <div class="right-button"> > </div>
   </div>
 */
+
+let images = [
+  "./assets/carousel/mountains.jpeg",
+  "./assets/carousel/computer.jpeg",
+  "./assets/carousel/trees.jpeg",
+  "./assets/carousel/turntable.jpeg"
+]
+
+function carouselCreator() {
+  let carousel = document.createElement("div");
+  carousel.classList = "carousel";
+
+  let leftButton = document.createElement("div");
+  leftButton.classList = "left-button";
+  carousel.appendChild(leftButton);
+
+  images.forEach(image => {
+    let img = document.createElement("img");
+    img.src = image;
+    carousel.appendChild(img);
+  })
+
+  let rightButton = document.createElement("div");
+  rightButton.classList = "right-button";
+  carousel.appendChild(rightButton);
+
+  console.log(carousel);
+  return carousel;
+
+}
+
+document.querySelector(".carousel-container").appendChild(carouselCreator());
+
+//indexOf function can grab index of src of current image
+
+//display property is set to "none" in CSS, need to update that to display: block for image that is displayed
+
+//event listener for left button subtracts 1 from index of current image
+
+//event listener for right button adds 1 to index of current image
